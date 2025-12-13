ed8c7b1ffdbab8d0fd6d62cf6d70882d2de16dd9
Anton Vix <Gordan.ei@protonmail.ch>
2025-12-13 20:12:07 +0400

    refactor: remove unused path import from extension test file

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,6 +1,5 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
-import * as path from 'path';
 
 suite('PHP Parameter Hints Test Suite', () => {
     test('Extension should be present', () => {
