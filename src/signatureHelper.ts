939de312cc35c94612bc707c906ea6540d833b15
Anton Vix <Gordan.ei@protonmail.ch>
2025-12-13 18:34:08 +0400

    add signature help function to retrieve parameter hints from language server

---
+++ src/signatureHelper.ts
@@ -0,0 +1,22 @@
+import * as vscode from 'vscode';
+
+/**
+ * Get signature help from language server (Intelephense) for a given position
+ */
+export async function getSignatureHelp(
+    document: vscode.TextDocument,
+    position: vscode.Position
+): Promise<vscode.SignatureHelp | undefined> {
+    try {
+        const signatureHelp = await vscode.commands.executeCommand<vscode.SignatureHelp>(
+            'vscode.executeSignatureHelpProvider',
+            document.uri,
+            position
+        );
+
+        return signatureHelp;
+    } catch (error) {
+        console.error('Error getting signature help:', error);
+        return undefined;
+    }
+}
