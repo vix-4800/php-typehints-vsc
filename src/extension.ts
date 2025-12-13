5f8815ea0cb27fa897f29b51edb890a92970a795
Anton Vix <Gordan.ei@protonmail.ch>
2025-12-13 18:35:58 +0400

    refactor activate function to initialize PhpInlayHintsProvider and register inlay hints for PHP

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,14 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { PhpInlayHintsProvider } from './inlayHintsProvider';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "php-typehints-vsc" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    const disposable = vscode.commands.registerCommand('php-typehints-vsc.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from php-typehints-vsc!');
-    });
+    const provider = new PhpInlayHintsProvider();
+    const disposable = vscode.languages.registerInlayHintsProvider(
+        { language: 'php', scheme: 'file' },
+        provider
+    );
 
     context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
