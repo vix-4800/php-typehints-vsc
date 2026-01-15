f051c1dc421106bd29b4bfadcce449b403a42816
Anton Vix <Gordan.ei@protonmail.ch>
2026-01-15 17:09:16 +0400

    feat: add User and Model classes with nullable createdBy property

--- test-fixtures/07-nullsafe.php
+++ test-fixtures/07-nullsafe.php
@@ -13,4 +13,24 @@ class TwoCaptcha {
     }
 }
 
+class User {
+    public $uniqid;
+    public function getShortName(): string {
+        return 'User';
+    }
+}
+
+class Model {
+    public ?User $createdBy = null;
+}
+
+class Html {
+    public static function a(string $text, array $url): string {
+        return '<a href="#">' . $text . '</a>';
+    }
+}
+
 $solver = new TwoCaptcha(Config::findOne(['key' => '2captcha/auth-key'])?->value);
+
+$model = new Model();
+$result = Html::a($model->createdBy?->getShortName(), ['/user/view', 'id' => $model->createdBy->uniqid]);
